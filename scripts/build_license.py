b31fef0403d89991f7b9115815d24e000b9a0f05
TW0hank0 <248451438+TW0hank0@users.noreply.github.com>
2026-03-16 17:24:11 +0800

    rewrite build license script

--- scripts/build_license.py
+++ scripts/build_license.py
@@ -1,61 +1,124 @@
+import os
 import subprocess
 import sys
+import json
 
 
 def main():
     print("-" * 10, "cargo-about", "-" * 10)
-    command = [
-        "cargo",
-        "about",
-        "generate",
-        "--output-file",
-        "auto_generated/ThirdPartyLicense-Rust.html",
-        "--threshold",
-        "1.0",
-        "templates/about_html.hbs",
+    #
+    all_commands = [
+        [
+            "cargo",
+            "about",
+            "generate",
+            "--output-file",
+            "auto_generated/ThirdPartyLicense-Rust.html",
+            "--threshold",
+            "1.0",
+            "templates/about_html.hbs",
+        ],
+        [
+            "cargo-about",
+            "generate",
+            "--threshold",
+            "1.0",
+            "--output-file",
+            "auto_generated/ThirdPartyLicense-Rust.json",
+            "templates/about_json.hbs",
+        ],
+        [
+            "cargo-about",
+            "generate",
+            "--output-file",
+            "auto_generated/ThirdPartyLicense-Rust.md",
+            "--threshold",
+            "1.0",
+            "templates/about_markdown.hbs",
+        ],
     ]
-    print(f"Run Command: {' '.join(command)}")
-    subprocess.run(
-        command,
-        check=True,
-        stdout=sys.stdout,
-        stdin=sys.stdin,
-        stderr=sys.stderr,
+    #
+    for command in all_commands:
+        print(f"Run Command: {' '.join(command)}")
+        subprocess.run(
+            command,
+            check=True,
+            stdout=sys.stdout,
+            stdin=sys.stdin,
+            stderr=sys.stderr,
+        )
+    #
+    print("Indenting json file...")
+    json_file_path = os.path.abspath(
+        os.path.join(
+            __file__,
+            "..",
+            "..",
+            "auto_generated",
+            "ThirdPartyLicense-Rust.json",
+        )
     )
-    command2 = [
-        "cargo-about",
-        "generate",
-        "--threshold",
-        "1.0",
-        "--output-file",
-        "auto_generated/ThirdPartyLicense-Rust.json",
-        "templates/about_json.hbs",
-    ]
-    print(f"Run Command: {' '.join(command2)}")
-    subprocess.run(
-        command2,
-        check=True,
-        stdout=sys.stdout,
-        stdin=sys.stdin,
-        stderr=sys.stderr,
-    )
-    command3 = [
-        "cargo-about",
-        "generate",
-        "--output-file",
-        "auto_generated/ThirdPartyLicense-Rust.md",
-        "--threshold",
-        "1.0",
-        "templates/about_markdown.hbs",
-    ]
-    print(f"Run Command: {' '.join(command3)}")
-    subprocess.run(
-        command3,
-        check=True,
-        stdout=sys.stdout,
-        stdin=sys.stdin,
-        stderr=sys.stderr,
-    )
+    with open(json_file_path, "r", encoding="utf-8") as f:
+        json_data = json.load(f)
+    with open(json_file_path, "w", encoding="utf-8") as f:
+        json.dump(json_data, f, ensure_ascii=False, sort_keys=True, indent=4)
+    #
+    # command = [
+    #     "cargo",
+    #     "about",
+    #     "generate",
+    #     "--output-file",
+    #     "auto_generated/ThirdPartyLicense-Rust.html",
+    #     "--threshold",
+    #     "1.0",
+    #     "templates/about_html.hbs",
+    # ]
+    # print(f"Run Command: {' '.join(command)}")
+    # subprocess.run(
+    #     command,
+    #     check=True,
+    #     stdout=sys.stdout,
+    #     stdin=sys.stdin,
+    #     stderr=sys.stderr,
+    # )
+    # #
+    # command2 = [
+    #     "cargo-about",
+    #     "generate",
+    #     "--threshold",
+    #     "1.0",
+    #     "--output-file",
+    #     "auto_generated/ThirdPartyLicense-Rust.json",
+    #     "templates/about_json.hbs",
+    # ]
+    # print(f"Run Command: {' '.join(command2)}")
+    # subprocess.run(
+    #     command2,
+    #     check=True,
+    #     stdout=sys.stdout,
+    #     stdin=sys.stdin,
+    #     stderr=sys.stderr,
+    # )
+
+    # #
+    # command3 = [
+    #     "cargo-about",
+    #     "generate",
+    #     "--output-file",
+    #     "auto_generated/ThirdPartyLicense-Rust.md",
+    #     "--threshold",
+    #     "1.0",
+    #     "templates/about_markdown.hbs",
+    # ]
+    # print(f"Run Command: {' '.join(command3)}")
+    # subprocess.run(
+    #     command3,
+    #     check=True,
+    #     stdout=sys.stdout,
+    #     stdin=sys.stdin,
+    #     stderr=sys.stderr,
+    # )
+    #
 
 
 if __name__ == "__main__":
