fbf884871e86c852a41f77f3c8c392f73492aeee
TW0hank0 <public.email.hank@gmail.com>
2026-04-04 12:38:40 +0800

    fix workflow(ci) (#60)

--- scripts/ci/rename_apk.py
+++ scripts/ci/rename_apk.py
@@ -3,7 +3,14 @@ import sys
 
 
 def main():
-    apk_path = os.path.join(__file__, "..", "..", "..", "target", "release", "apk")
+    apk_path = os.path.abspath(
+        os.path.join(
+            os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
+            "target",
+            "release",
+            "apk",
+        )
+    )
     orig_name = os.path.join(apk_path, "pmj_client.apk")
     new_name = os.path.join(apk_path, f"pmj_client_{sys.argv[1]}.apk")
     os.rename(orig_name, new_name)
