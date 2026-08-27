51eee53073d5444218d96f47b8efd21aee85a7e5
TW0hank0 <public.email.hank@tutamail.com>
2026-08-27 14:58:26 +0800

    dev website

--- scripts/util.py
+++ scripts/util.py
@@ -15,11 +15,13 @@
 
 """`positive_mahjong` script util"""
 
+import datetime
 import os
 import subprocess
 import sys
 import tomllib
 from typing import Literal
+from zoneinfo import ZoneInfo
 
 from colorama import Back, Fore, Style
 
@@ -37,11 +39,11 @@ def run_cmd(command: list[str], cwd: str | None = None) -> tuple[int, str]:
     )
     if process.returncode == 0:
         print(
-            f"{Style.DIM}=>{Style.NORMAL} {Fore.GREEN}Process fnished sucessful.{Fore.RESET}"
+            f"{Fore.LIGHTBLACK_EX}=>{Fore.RESET} {Fore.GREEN}Process fnished sucessful.{Fore.RESET}"
         )
     else:
         print(
-            f"{Style.DIM}=>{Style.NORMAL} {Fore.RED}Process exited with non-zero code{Fore.RESET} {Style.DIM}({process.returncode}){Style.NORMAL}!"
+            f"{Fore.LIGHTBLACK_EX}=>{Fore.RESET} {Fore.RED}Process exited with non-zero code{Fore.RESET} {Style.DIM}({process.returncode}){Style.NORMAL}!"
         )
         for name, data in [
             ("stdout", process.stdout.decode()),
@@ -49,29 +51,76 @@ def run_cmd(command: list[str], cwd: str | None = None) -> tuple[int, str]:
         ]:
             print(f"{Style.DIM}---{Style.NORMAL} {name}")
             for line in data.split("\n"):
-                print(
-                    f" {Style.DIM}{Fore.LIGHTBLACK_EX}|{Fore.RESET}{Style.NORMAL} {line}"
-                )
-            print(f"{Style.DIM}---{Style.NORMAL} end-of {name}")
+                print(f" {Fore.LIGHTBLACK_EX}|{Fore.RESET} {line}")
+            print(f"{Fore.LIGHTBLACK_EX}---{Fore.RESET} end-of {name}")
         sys.exit(1)
-    return (process.returncode, process.stdout.decode())
+    return (process.returncode, process.stdout.decode().rstrip("\n"))
 
 
 def get_commit_info():
-    """return (commit_sha, commit_time, commit_msg)"""
+    """return CommitInfo"""
     (_returncode, stdout) = run_cmd(["git", "rev-parse", "HEAD"], cwd=fix_path())
     commit_sha = stdout.replace("\n", "")
     (_returncode, commit_time) = run_cmd(
-        ["git", "log", "-1", '--format="%cd"', "--date=iso"], cwd=fix_path()
+        ["git", "log", "-1", "--format=%cd", "--date=iso"], cwd=fix_path()
     )
     (_returncode, commit_msg) = run_cmd(
         ["git", "log", "-n", "1", "--format=%B"], cwd=fix_path()
     )
-    return (commit_sha, commit_time, commit_msg)
+    (_, commit_committer_name) = run_cmd(["git", "log", "-1", "--format=%cn"])
+    (_, commit_committer_email) = run_cmd(["git", "log", "-1", "--format=%ce"])
+    (_, commit_author_name) = run_cmd(["git", "log", "-1", "--format=%an"])
+    (_, commit_author_email) = run_cmd(["git", "log", "-1", "--format=%ae"])
+    return CommitInfo(
+        commit_sha,
+        commit_time,
+        commit_msg,
+        commit_committer_name,
+        commit_committer_email,
+        commit_author_name,
+        commit_author_email,
+    )
+
+
+class CommitInfo:
+    __slots__: list[str] = [
+        "sha",
+        "time",
+        "msg",
+        "committer_name",
+        "committer_email",
+        "author_name",
+        "author_email",
+    ]
+    sha: str
+    time: str
+    msg: str
+    committer_name: str
+    committer_email: str
+    author_name: str
+    author_email: str
+
+    def __init__(
+        self,
+        commit_sha: str,
+        commit_time: str,
+        commit_msg: str,
+        committer_name: str,
+        committer_email: str,
+        author_name: str,
+        author_email: str,
+    ) -> None:
+        self.sha = commit_sha
+        self.time = commit_time
+        self.msg = commit_msg
+        self.committer_name = committer_name
+        self.committer_email = committer_email
+        self.author_name = author_name
+        self.author_email = author_email
 
 
 def fix_path(*p: str) -> str:
-    return os.path.join(os.path.dirname(os.path.dirname(__file__)), *p)
+    return os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), *p))
 
 
 def get_version(workspace_type: Literal["workspace", "package"] = "workspace") -> str:
@@ -86,6 +135,11 @@ def get_version(workspace_type: Literal["workspace", "package"] = "workspace") -
         case "workspace":
             version = str(data["workspace"]["package"]["version"])
     return version
+
+
+def get_datetime():
+    tz = ZoneInfo("Asia/Taipei")
+    return datetime.datetime.now(tz).strftime("%Y-%m-%d_%H-%M-%S")
 
 
 if __name__ == "__main__":
