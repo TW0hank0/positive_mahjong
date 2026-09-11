a0cbaade043bde87f69e1bb3dea57a082eb760f8
TW0hank0 <public.email.hank@tutamail.com>
2026-09-11 17:27:33 +0800

    change msrv to use util.py

--- scripts/util.py
+++ scripts/util.py
@@ -23,7 +23,6 @@ import sys
 import time
 import tomllib
 from typing import Literal
-from zoneinfo import ZoneInfo
 
 from colorama import Back, Fore
 
@@ -200,8 +199,7 @@ def get_version(
 
 
 def get_datetime():
-    tz = ZoneInfo("Asia/Taipei")
-    return datetime.datetime.now(tz).strftime("%Y-%m-%d_%H-%M-%S")
+    return datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%d_%H-%M-%S")
 
 
 def list_files(path: str) -> list[str]:
@@ -231,6 +229,7 @@ if __name__ == "__main__":
             "commit_sha",
             "commit_short_sha",
             "get_commit_msg",
+            "msrv",
         ],
     ):
         match command:
@@ -247,5 +246,14 @@ if __name__ == "__main__":
             case "get_commit_msg":
                 commit_info = get_commit_info()
                 print(commit_info.msg)
+            case "msrv":
+                _ = run_cmd(
+                    ["cargo", "msrv", "find", "--", "--workspace"],
+                    cwd=fix_path(),
+                )
+                _ = run_cmd(
+                    ["cargo", "msrv", "verify", "--", "--workspace"],
+                    cwd=fix_path(),
+                )
 
     app()
